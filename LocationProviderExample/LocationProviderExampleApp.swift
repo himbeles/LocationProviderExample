47e3f2d0b9a1b9f475d4f1828304979cf5b9025b
Luis Riegger <lri@me.com>
2020-09-06 10:01:07 +0200

    Initial Commit

---
+++ LocationProviderExample/LocationProviderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationProviderExampleApp.swift
+//  LocationProviderExample
+//
+//  Created by Luis on 06.09.20.
+//
+
+import SwiftUI
+
+@main
+struct LocationProviderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
