9bde4357935db0e60ed77926262fb1b23745ced6
Luis Riegger <lri@me.com>
2020-09-06 10:09:25 +0200

    Minimal working example

--- LocationProviderExample/ContentView.swift
+++ LocationProviderExample/ContentView.swift
@@ -6,11 +6,25 @@
 //
 
 import SwiftUI
+import LocationProvider
 
 struct ContentView: View {
+    @ObservedObject var locationProvider : LocationProvider
+    
+    init() {
+        locationProvider = LocationProvider()
+        do {try locationProvider.start()}
+        catch {
+            print("No location access.")
+            locationProvider.requestAuthorization()
+        }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        Text("latitude \(locationProvider.location?.coordinate.latitude ?? 0)")
+        Text("longitude \(locationProvider.location?.coordinate.longitude ?? 0)")
+        }
     }
 }
 
